3f8a462af1ff004ff9e4d67e4ab58fa208f0b077
Preeti Das <preetidas_a@yahoo.com>
2021-12-16 13:41:16 -0600

    added some more features

--- handlebars/starting-point/models/restaurant.js
+++ handlebars/starting-point/models/restaurant.js
@@ -5,7 +5,9 @@ class Restaurant extends Model {}
 Restaurant.init({
     name: DataTypes.STRING,
     image: DataTypes.STRING,
-}, {
+    },
+    
+{
     sequelize: db,
     timestamps: false,
 });
