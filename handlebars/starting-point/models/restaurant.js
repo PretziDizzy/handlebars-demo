3b87089cd144ef3a79eb476899db6f01e651da34
Preeti Das <preetidas_a@yahoo.com>
2021-12-16 16:14:42 -0600

    populated DB

--- handlebars/starting-point/models/restaurant.js
+++ handlebars/starting-point/models/restaurant.js
@@ -3,8 +3,19 @@ const { DataTypes, Model } = require('sequelize');
 class Restaurant extends Model {}
 
 Restaurant.init({
-    name: DataTypes.STRING,
-    image: DataTypes.STRING,
+    name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+    },
+    image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+    },
+
+    likes: {type: DataTypes.INTEGER, 
+        defaultValue: 0}
     },
     
 {
